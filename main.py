970644de36bb90268245ba727e424015f42301b0
Hassan Shahzad <chhxnshah@gmail.com>
2022-01-07 13:08:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from moviepy.editor import *
 
 
 def convert_mp4_to_jpgs(path):
-    clip = VideoFileClip(path).resize(0.2)
-    clip.write_gif("obscure.gif")
+    clip = VideoFileClip(path).resize(0.3)
+    clip.write_gif("garbled.gif")
 
 
 if __name__ == "__main__":
-    convert_mp4_to_jpgs("Obscure.mp4")
+    convert_mp4_to_jpgs("Garbled Image.mp4")
